c9e6466aa4075fcbefea2bb7ec6d892391c4800f
신현섭 <didakd22@naver.com>
2025-06-25 14:33:53 +0900

    chapter 3-8 완료

--- src/main/java/com/example/oauth/common/auth/JwtTokenProvider.java
+++ src/main/java/com/example/oauth/common/auth/JwtTokenProvider.java
@@ -1,5 +1,46 @@
 package com.example.oauth.common.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+
 // JWT 발급 처리
+@Component
 public class JwtTokenProvider {
+
+    private final String secretKey;
+
+    private final int expiration;
+
+    private Key SECRET_KEY;
+
+    public JwtTokenProvider(@Value("${jwt.secret}") String secretKey, @Value("${jwt.expiration}") int expiration) {
+        this.secretKey = secretKey;
+        this.expiration = expiration;
+        // 시크릿키는 yml 파일에서 인코딩 된 채로 일단 들어가 있음. 인코딩된 시크릿키를 디코딩 해준 다음 암호화.
+        // JWT 세번째 파트 부분에 들어갈 시크릿키를 암호화해주는 코드임.
+        this.SECRET_KEY = new SecretKeySpec(java.util.Base64.getDecoder().decode(secretKey), SignatureAlgorithm.HS512.getJcaName());
+    }
+
+    public String createToken(String email, String role) {
+        // Claims는 jwt 토큰의 payload 부분을 의미한다.
+        Claims claims = Jwts.claims().setSubject(email);
+        claims.put("role", role);
+        Date now = new Date();
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + expiration * 60 * 1000L))
+                // signWith는 시그니처 부분임. 시크릿키를 넣어주자.
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS512)
+                .compact();
+
+        return token;
+    }
 }
--- src/main/java/com/example/oauth/member/controller/MemberController.java
+++ src/main/java/com/example/oauth/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.oauth.member.controller;
 
+import com.example.oauth.common.auth.JwtTokenProvider;
 import com.example.oauth.member.domain.Member;
 import com.example.oauth.member.dto.MemberCreateDto;
 import com.example.oauth.member.dto.MemberLoginDto;
@@ -11,13 +12,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, JwtTokenProvider jwtTokenProvider) {
         this.memberService = memberService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     // 회원가입
@@ -34,6 +40,11 @@ public class MemberController {
         Member member = memberService.login(memberLoginDto);
         
         // 일치할 경우 jwt access token 생성
-        return null;
+        String jwtToken = jwtTokenProvider.createToken(member.getEmail(), member.getRole().toString());
+
+        Map<String, Object> loginInfo = new HashMap<>();
+        loginInfo.put("id", member.getId());
+        loginInfo.put("token", jwtToken);
+        return new ResponseEntity<>(loginInfo, HttpStatus.OK);
     }
 }
