63769a418b05400782de536c604624fb8cf0940e
신현섭 <didakd22@naver.com>
2025-06-25 18:15:51 +0900

    chapter 3-9 완료

--- src/main/java/com/example/oauth/common/auth/JwtTokenFilter.java
+++ src/main/java/com/example/oauth/common/auth/JwtTokenFilter.java
@@ -1,5 +1,17 @@
 package com.example.oauth.common.auth;
 
+import jakarta.servlet.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
 // JWT 검증 처리
-public class JwtTokenFilter {
+@Component
+public class JwtTokenFilter extends GenericFilter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response
+            , FilterChain chain) throws IOException, ServletException {
+
+        chain.doFilter(request, response);
+    }
 }
--- src/main/java/com/example/oauth/common/config/SecurityConfig.java
+++ src/main/java/com/example/oauth/common/config/SecurityConfig.java
@@ -2,8 +2,17 @@ package com.example.oauth.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -12,6 +21,44 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder makePassword() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain myfilter(HttpSecurity httpSecurity) throws Exception{
+
+        return httpSecurity
+                // cors는 프런트 화면이 있을 때 의미가 있는 설정인데 같은 도메인 끼리만 api를 통해서 데이터를 주고받겠다라는 설정이다.
+                // 우린 서버는 8080 프런트는 3000 으로 설정할 거임. 두 개가 서로 도메인이 다름. 그래서 3000번 같은 경우에는 허용하겠다고 설정을 해줘야 함.
+                .cors(cors -> cors.configurationSource(configurationSource()))
+                // csrf 비활성화
+                // csrf 라고 하는 것은 보안 공격 중 하나인데 보통 mvc 패턴에서 많이 사용되는 공격이다.
+                // 우리는 mvc 패턴이 아니고 restful 설계가 되어 있고, 프런트가 별도로 있는 구조라서 csrf는 따로 설정하지 않겠다.
+                .csrf(AbstractHttpConfigurer::disable)
+                // Basic 인증 비활성화 -> 사용자 이름과 비밀번호를 Base64로 인코딩 하여 인증값으로 활용 -> 우리는 토큰 인증 방식을 사용할 것이라서 비활성화 함.
+                .httpBasic(AbstractHttpConfigurer::disable)
+                // 세션방식을 비활성화 -> 로그인 방식은 크게 두 가지로 나뉠 수 있는데 세션 방식과 토큰 방식임.
+                // 세션 방식은 로그인 인증 값을 서버에서 메모리 값으로 가지고 있는 것.
+                // 토큰 방식은 서버에서 별도로 저장을 하지 않음.
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // 특정 url 패턴에 대해서는 인증 처리 제외
+                // 인증 처리란 Authentication 객체를 생성하는 것을 말한다.
+                .authorizeHttpRequests(a -> a.requestMatchers("/member/create", "/member/doLogin").permitAll().anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 특정 도메인 허용
+        configuration.setAllowedMethods(Arrays.asList("*")); // 모든 HTTP 메서드 허용
+        configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더값을 허용
+        configuration.setAllowCredentials(true); // 자격 증명 허용 -> Authrization 헤더를 사용할 수 있도록 설정하는 것
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // 모든 url 패턴에 대해서 cors 허용 설정
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
 }
