61d8d8a3c0ae2f3e552b0864ae5bcec4593228eb
신현섭 <didakd22@naver.com>
2025-06-26 22:32:19 +0900

    chapter 3-10 비밀번호 설명 주석 추가

--- src/main/java/com/example/oauth/member/domain/Member.java
+++ src/main/java/com/example/oauth/member/domain/Member.java
@@ -21,6 +21,7 @@ public class Member {
     @Column(nullable = false, unique = true)
     private String email;
 
+    // 비밀번호는 일반 로그인에만 필요하며, SNS 사용자는 비밀번호가 없다. 따라서 nullable로 설계되었음. 이메일은 필수.
     private String password;
 
     // Enum을 String 타입으로 지정 안할시 숫자로 DB에 저장된다. 문자열로 저장하기 위해 EnumType.STRING 지정.
